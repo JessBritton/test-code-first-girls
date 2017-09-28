e09d5b206ea15866feeaa5e441a7fa71135bd919
JessBritton <Jess.britton95>
2017-09-28 16:30:20 +0100

    Added homeBanner and menu

--- index/Javascript.js
+++ index/Javascript.js
@@ -1,10 +1,14 @@
 function openNav() {
     document.getElementById("mySidenav").style.maxWidth = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+
 
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.maxWidth = "0";
+    document.getElementById("main").style.marginLeft= "0";
+   
 
 }
